4ce35a4dcccb37b7fe5d8fef8f5c0046bd9b046d
Sumio K <tap.miwo@gmail.com>
2022-07-31 18:45:56 +0900

    create comments model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_070258) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_31_094120) do
+  create_table "comments", force: :cascade do |t|
+    t.string "comment", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
