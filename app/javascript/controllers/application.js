408118064a45e5c88a7287dc454ef266af7ac611
Sumio K <tap.miwo@gmail.com>
2022-08-21 21:56:18 +0900

    add posts preview

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,51 +8,36 @@ window.Stimulus   = application
 
 export { application }
 
-(()=>{
-    const $doc = document;
-    const $tab = $doc.getElementById("js-tab");
-    const $nav = $tab.querySelectorAll('[data-nav]');
-    const $content = $tab.querySelectorAll('[data-content]')
-    const ACTIVE_CLASS = 'is-active'
-    const navLength = $nav.length
 
+if (document.URL.match(/new/)){
+    document.addEventListener('DOMContentLoaded', () => {
+        const createImageHTML = (blob) => {
+        const imageElement = document.getElementById('new-image');
+        const blobImage = document.createElement('img');
+        blobImage.setAttribute('class', 'new-img')
+        blobImage.setAttribute('src', blob);
 
-    const init =() =>{
-        $content[0].style.display = "flex";
-    }
-    init();
+        imageElement.appendChild(blobImage);
+        };
 
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        
-        const $this = e.target;
-        const targetValue = $this.dataset.nav
-
-        let index = 0;
-        while(index < navLength) {
-            $content[index].style.display = 'none';
-            $nav[index].classList.remove(ACTIVE_CLASS)
-            index++;
+        document.getElementById('post-img').addEventListener('change', (e) => {
+        const imageContent = document.querySelector('.new-img');
+        if (imageContent){
+            imageContent.remove();
         }
 
-        $tab.querySelectorAll('[data-content="' + targetValue + '"]')[0].style.display = "flex"
-        $nav[targetValue].classList.add(ACTIVE_CLASS)
-        
-    }
-    
-    let index = 0;
-    while(index < navLength){
-    $nav[index].addEventListener('click' , (e) => handleClick(e));
-    index ++;
-    }
+        const file = e.target.files[0];
+        const blob = window.URL.createObjectURL(file);
+        createImageHTML(blob);
+        });
+    });
+}
 
-})();
 
 
 // (()=>{
 //     const $doc = document;
-//     const $tab = $doc.getElementById("js-post");
+//     const $tab = $doc.getElementById("js-tab");
 //     const $nav = $tab.querySelectorAll('[data-nav]');
 //     const $content = $tab.querySelectorAll('[data-content]')
 //     const ACTIVE_CLASS = 'is-active'
