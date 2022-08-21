d1696bb43bf7a539b436ac10c3a4e9bebe9da8e7
Sumio K <tap.miwo@gmail.com>
2022-08-22 06:07:14 +0900

    tab.js limited

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -61,43 +61,44 @@ if (document.URL.match(/edit/)){
 }
 
 
-
-(()=>{
-    const $doc = document;
-    const $tab = $doc.getElementById("js-tab");
-    const $nav = $tab.querySelectorAll('[data-nav]');
-    const $content = $tab.querySelectorAll('[data-content]')
-    const ACTIVE_CLASS = 'is-active'
-    const navLength = $nav.length
-
-
-    const init =() =>{
-        $content[0].style.display = "flex";
-    }
-    init();
+if (document.URL.match(/users/)){
+    (()=>{
+        const $doc = document;
+        const $tab = $doc.getElementById("js-tab");
+        const $nav = $tab.querySelectorAll('[data-nav]');
+        const $content = $tab.querySelectorAll('[data-content]')
+        const ACTIVE_CLASS = 'is-active'
+        const navLength = $nav.length
 
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        
-        const $this = e.target;
-        const targetValue = $this.dataset.nav
-
-        let index = 0;
-        while(index < navLength) {
-            $content[index].style.display = 'none';
-            $nav[index].classList.remove(ACTIVE_CLASS)
-            index++;
+        const init =() =>{
+            $content[0].style.display = "flex";
         }
+        init();
 
-        $tab.querySelectorAll('[data-content="' + targetValue + '"]')[0].style.display = "flex"
-        $nav[targetValue].classList.add(ACTIVE_CLASS)
+
+        const handleClick = (e) => {
+            e.preventDefault();
+            
+            const $this = e.target;
+            const targetValue = $this.dataset.nav
+
+            let index = 0;
+            while(index < navLength) {
+                $content[index].style.display = 'none';
+                $nav[index].classList.remove(ACTIVE_CLASS)
+                index++;
+            }
+
+            $tab.querySelectorAll('[data-content="' + targetValue + '"]')[0].style.display = "flex"
+            $nav[targetValue].classList.add(ACTIVE_CLASS)
+            
+        }
         
-    }
-    
-    let index = 0;
-    while(index < navLength){
-    $nav[index].addEventListener('click' , (e) => handleClick(e));
-    index ++;
-    }
-})();
+        let index = 0;
+        while(index < navLength){
+        $nav[index].addEventListener('click' , (e) => handleClick(e));
+        index ++;
+        }
+    })();
+}
