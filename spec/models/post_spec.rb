4aafca6d7611e9a2d2004f3c0def8559e78e3d71
Sumio K <tap.miwo@gmail.com>
2022-08-24 21:04:16 +0900

    add test

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it "post can post" do
+    user = User.create(
+      id: 1,
+      name: "Jack",
+      email: "jack@example.com",
+      password: "password",
+      introduction: "test"
+  )
+    post = Post.new(
+      user_id: 1,
+      image: "sample.jpg",
+      content: "test"
+    )
+    expect(post).to be_valid
+  end
+  
+  it "can not post without images" do
+    user = User.create(
+      id: 1,
+      name: "Jack",
+      email: "jack@example.com",
+      password: "password",
+      introduction: "test"
+  )
+    post = Post.new(
+      user_id: 1,
+      image: nil,
+      content: "test"
+    )
+    expect(post).to be_invalid
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,60 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    before do
-        @user = FactoryBot.build(:user)
+    describe 'valid' do
+        it "is valid with a name ,email, password, password2" do
+            user = User.new(
+                name: "Jack",
+                email: "jack@example.com",
+                password: "password"
+            )
+            expect(user).to be_valid
+        end
+
+        it "is inavalid without a name" do
+            user = User.new(
+                name: nil
+            )
+            expect(user).to be_invalid
+            expect(user.errors[:name]).to include("can't be blank")
+        end
+
+        it "is inavalid without a email address" do
+            user = User.new(
+                email: nil
+            )
+            expect(user).to be_invalid
+            expect(user.errors[:name]).to include("can't be blank")
+        end
+        
+        it "is invalid without a fraudulent email address" do
+            user = User.new(
+                name: "Jack",
+                email: "jackexample.com",
+                password: "password"
+            )
+            expect(user).to be_invalid
+        end
+
+        it "is invalid with a duplicate email" do
+            User.create(
+                name: "Jack",
+                email: "jack@example.com",
+                password: "password"
+            )
+            user = User.new(
+                name: "Jhoe",
+                email: "jack@example.com",
+                password: "password"
+            )
+            user.valid?
+            expect(user.errors[:email]).to include("has already been taken")
+        end
+    end
+    describe 'check true value' do
+        it "name is true" do
+            user = User.create(
+                name: "Jack",
+                email: "jack@example.com",
+                password: "password",
+                introduction: "test"
+            )
+            expect(user.name).to eq "Jack"
+        end
+        it "email is true" do
+            user = User.create(
+                name: "Jack",
+                email: "jack@example.com",
+                password: "password",
+                introduction: "test"
+            )
+            expect(user.name).to eq "Jack"
+        end
+        it "introduction is true" do
+            user = User.create(
+                name: "Jack",
+                email: "jack@example.com",
+                password: "password",
+                introduction: "test"
+            )
+            expect(user.name).to eq "Jack"
+        end
+        it "password is true" do
+            user = User.create(
+                name: "Jack",
+                email: "jack@example.com",
+                password: "password",
+                introduction: "test"
+            )
+            expect(user.name).to eq "Jack"
+        end
+
     end
 
-    it "is calid with a email and password" do
-        expect(@user).to be_valid
-    end
-
-    it "is valid with a name ,email, password, password2" do
-        user = User.new(
-            name: "Jack",
-            email: "jack@example.com",
-            password: "password"
-        )
-        expect(user).to be_valid
-    end
-
-    it "is inavalid without a name" do
-        user = User.new(
-            name: nil
-        )
-        expect(user).to be_invalid
-        expect(user.errors[:name]).to include("can't be blank")
-    end
-
-    it "is inavalid without a email address" do
-        user = User.new(
-            email: nil
-        )
-        expect(user).to be_invalid
-        expect(user.errors[:name]).to include("can't be blank")
-    end
-    
-    it "is invalid without a fraudulent email address" do
-        user = User.new(
-            name: "Jack",
-            email: "jackexample.com",
-            password: "password"
-        )
-        expect(user).to be_invalid
-    end
-    
-    it "is invalid with a duplicate email" do
-        User.create(
-            name: "Jack",
-            email: "jack@example.com",
-            password: "password"
-        )
-        user = User.new(
-            name: "Jhoe",
-            email: "jack@example.com",
-            password: "password"
-        )
-        user.valid?
-        expect(user.errors[:email]).to include("has already been taken")
-    end
 end
