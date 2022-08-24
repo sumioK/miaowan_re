e06ae9094946116204543d88640aeadb63990df0
Sumio K <tap.miwo@gmail.com>
2022-08-24 18:48:45 +0900

    valid test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,24 @@ RSpec.describe User, type: :model do
 
     it "is calid with a email and password" do
         expect(@user).to be_valid
-
     end
+
+    it "is valid with a name ,email, password, password2" do
+        user = User.new(
+            name: "Jack",
+            email: "jack@example.com",
+            password: "password"
+        )
+        expect(user).to be_valid
+    end
+    it "is inavalid without a name" do
+        user = User.new(name: nil)
+        user.valid?
+        expect(user.errors[:name].to include("can't be blank"))
+    end
+
+    it "is inavalid without a email address"
+
+    it "is invalid with a duplicate email"
+
 end
