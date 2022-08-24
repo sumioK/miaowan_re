d658c17e304fdc15ed97fa8ac6bc099050d9210c
Sumio K <tap.miwo@gmail.com>
2022-08-24 20:07:06 +0900

    email need to include @

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 validates :introduction, length: {maximum: 250}
 validates :name, presence: true
+validates :email, presence: true ,uniqueness: true, format: {with: /@/,message:"不正なメールアドレスです"}
 validates :password, presence: true
 
     has_many :posts, dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,5 +33,28 @@ RSpec.describe User, type: :model do
         expect(user).to be_invalid
         expect(user.errors[:name]).to include("can't be blank")
     end
-    it "is invalid with a duplicate email"
+    
+    it "is invalid without a fraudulent email address" do
+        user = User.new(
+            name: "Jack",
+            email: "jackexample.com",
+            password: "password"
+        )
+        expect(user).to be_invalid
+    end
+    
+    it "is invalid with a duplicate email" do
+        User.create(
+            name: "Jack",
+            email: "jack@example.com",
+            password: "password"
+        )
+        user = User.new(
+            name: "Jhoe",
+            email: "jack@example.com",
+            password: "password"
+        )
+        user.valid?
+        expect(user.errors[:email]).to include("has already been taken")
+    end
 end
