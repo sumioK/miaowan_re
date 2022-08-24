1d70cf992533cc131f553e6c5782d292c6e97498
Sumio K <tap.miwo@gmail.com>
2022-08-24 19:05:51 +0900

    valid test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,14 +17,21 @@ RSpec.describe User, type: :model do
         )
         expect(user).to be_valid
     end
+
     it "is inavalid without a name" do
-        user = User.new(name: nil)
-        user.valid?
-        expect(user.errors[:name].to include("can't be blank"))
+        user = User.new(
+            name: nil
+        )
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("can't be blank")
     end
 
-    it "is inavalid without a email address"
-
+    it "is inavalid without a email address" do
+        user = User.new(
+            email: nil
+        )
+        expect(user).to be_invalid
+        expect(user.errors[:name]).to include("can't be blank")
+    end
     it "is invalid with a duplicate email"
-
 end
