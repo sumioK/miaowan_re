8cb8b868b2cdb48aaf027653e5b26f1bf2bfb7eb
Sumio K <tap.miwo@gmail.com>
2022-08-25 21:33:07 +0900

    add image not change test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -33,7 +33,24 @@ RSpec.describe Post, type: :model do
     expect(post).to be_invalid
   end
 
-  it "if update's image is null image doesn't change"
-
+  it "if update's image is null image doesn't change" do
+    user = User.create(
+      id: 1,
+      name: "Jack",
+      email: "jack@example.com",
+      password: "password",
+      introduction: "test"
+    )
+    post = Post.new(
+      user_id: 1,
+      image: "sample.jpg",
+      content: "test"
+    )
+    post.update(
+      content: "changed"
+    )
+      expect(post.content).to eq "changed"
+      expect(post.image).to eq "sample.jpg"
+  end
   it "if update has image , image changes"
 end
