fd3f2aebc6b2334bb6f240dc947edae63dad8f23
Sumio K <tap.miwo@gmail.com>
2022-08-26 21:40:05 +0900

    add commnent valid

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,17 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  discribe "valid" do
+  describe "valid" do
 
-    it "can comments while comment"
+    it "can comments while comment" do
+      user = User.create(
+        id: 1,
+        name: "Jack",
+        email: "jack@example.com",
+        password: "password",
+        introduction: "test"
+      )
+      post = Post.create(
+        user_id: 1,
+        image: "sample.jpg",
+        content: "test"
+      )
+      comment = Comment.new(
+        comment: "test comment",
+        user_id: 1,
+        post_id: 1
+      )
+      expect(comment).to be_valid
+    end
 
-    it "can't comments without comment"
+    it "can't comments without comment" do
+      user = User.create(
+        id: 1,
+        name: "Jack",
+        email: "jack@example.com",
+        password: "password",
+        introduction: "test"
+      )
+      post = Post.create(
+        user_id: 1,
+        image: "sample.jpg",
+        content: "test"
+      )
+      comment = Comment.new(
+        comment: nil,
+        user_id: 1,
+        post_id: 1
+      )
+      expect(comment).to be_invalid
+    end
 
     it "can't updates without comment"
 
   end
 
-  discribe "check method" do
+  describe "check method" do
 
     it "destroy works"
 
