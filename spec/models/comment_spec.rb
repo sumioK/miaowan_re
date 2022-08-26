f99014a1385a0932f9020c4013de3cd6301ce998
Sumio K <tap.miwo@gmail.com>
2022-08-26 21:29:28 +0900

    comment test create

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  discribe "valid" do
+
+    it "can comments while comment"
+
+    it "can't comments without comment"
+
+    it "can't updates without comment"
+
+  end
+
+  discribe "check method" do
+
+    it "destroy works"
+
+  end
+end
