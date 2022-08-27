5633cd1dd8c0428bf9a2ddfadf394933859d999e
Sumio K <tap.miwo@gmail.com>
2022-08-27 20:36:08 +0900

    like create test ok

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "like create work truth" do
+    user = User.create(
+      name: "A",
+      email: "a@example.com",
+      password: "password"
+    )
+    user2 = User.create(
+      name: "B",
+      email: "b@example.com",
+      password: "password"
+    )
+    post = Post.create(
+      user_id: 2,
+      image: "image.jpg",
+    )
+    like = Like.create(
+      user_id: 1,
+      post_id: 2
+    )
+    expect(like).to be_valid
+  end
+
+  it "destroy like work truth"
 end
