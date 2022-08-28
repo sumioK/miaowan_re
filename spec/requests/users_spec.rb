4c07c7492e2ef18d6571f4598afbff4f6b85bad2
Sumio K <tap.miwo@gmail.com>
2022-08-28 15:40:22 +0900

    login request ok

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /signup" do
-    it "works! (now write some real specs)" do
+  describe "access users" do
+    it "access signup without login" do
       get "/signup"
       expect(response).to have_http_status(200)
     end
+
+    it "access login without login" do
+      get login_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "can not access index without login"
+
+    it "can not access show without login"
+
+    it "can not access edit without login"
+
+    it "can not access delete without login"
+
+    it "can not access logout without login"
   end
 end
