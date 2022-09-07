117605ff450d020d75afd3c009f9a0bec1473c65
Sumio K <tap.miwo@gmail.com>
2022-09-07 19:21:54 +0900

    fix alert

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -71,7 +71,7 @@ class PostsController < ApplicationController
   def ensure_correct_user
     @post =Post.find_by(id: params[:id])
     if @post.user_id != @current_user.id
-      flash[:notice] = "権限がありません"
+      flash[:alert] = "権限がありません"
       redirect_to("/posts/index")
     end
   end
